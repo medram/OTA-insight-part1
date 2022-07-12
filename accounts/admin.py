2ba1bc8b9ed56d7750d34620e6c87f6d0e35fc67
medram <mramouchy@gmail.com>
2022-07-12 16:20:09 +0100

    Updated: Django admin user is customized!

---
+++ accounts/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import Group
+from .models import CustomUser, CustomGroup
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_active',
+                    'is_staff', 'date_joined')
+    list_display_links = ('email',)
+    list_filter = ('is_active', 'is_staff', 'is_superuser',
+                   'updated', 'date_joined', 'last_login')
+    search_fields = ('email', 'username', 'name')
+    readonly_fields = ('change_password', 'updated',
+                       'date_joined', 'last_login')
+    filter_horizontal = ('user_permissions', 'groups')
+
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'name', 'company_name', 'country', 'currency', 'change_password')
+        }),
+        (_('Permissions & Groups'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        (_('More info'), {
+            'fields': ('updated', 'date_joined', 'last_login')
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser')
+        }),
+    )
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(CustomGroup)
+class GroupAdmin(GroupAdmin):
+    pass
---
+++ accounts/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group
+from django.utils.safestring import mark_safe
+from django_countries.fields import CountryField
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('Email is required!'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_active = extra_fields.get('is_active', True)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save()
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('Email address'), unique=True, db_index=True)
+    username = models.CharField(_('Username'), max_length=40, db_index=True)
+    name = models.CharField(
+        _('Full name'), max_length=30, blank=True, null=True)
+
+    company_name = models.CharField(
+        _('Company name'), max_length=30, null=True, blank=True)
+
+    country = CountryField(blank_label='(select country)')
+    # Could be e.g. '$' or 'USD'
+    currency = models.CharField(
+        max_length=3, default='$', help_text=_("Could be e.g. '$' or 'USD'"))
+
+    updated = models.DateTimeField(auto_now=True, db_index=True)
+    date_joined = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    # required fields
+    is_active = models.BooleanField(
+        _('Active'), default=True, help_text=_('Whether the user can login or not.'), db_index=True)
+    is_staff = models.BooleanField(_('Staff status'), default=False, help_text=_(
+        'Whether the user can login to this dashboard or not.'), db_index=True)
+    is_superuser = models.BooleanField(_('Superuser status'), default=False, help_text=_(
+        'Grant all permissions regardless of everything, without asigning them one by one.'), db_index=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        ordering = ('-date_joined',)
+
+    @admin.display(description=_('Password'))
+    def change_password(self, obj=None):
+        return mark_safe("<a href='../password' class='btn btn-primary btn-sm text-white'>%s</a>" % _('Change Password?'))
+
+    def __str__(self):
+        return self.email
+
+
+class CustomGroup(Group):
+
+    class Meta:
+        verbose_name = _('group')
